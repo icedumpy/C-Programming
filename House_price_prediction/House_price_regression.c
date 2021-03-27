77e503d42c3a4a78b2eb30b53b1e77ca6c9176bd
Pongporamat Charuchinda <pongporamat.c@ku.th>
2021-03-28 00:41:45 +0700

    Finish Simple Linear Regression with House price dataset

--- House_price_prediction/House_price_regression.c
+++ House_price_prediction/House_price_regression.c
@@ -157,44 +157,42 @@ float** load_data(char *file_name)
     return m;
 }
 
+void Normalize(float **m, int row, int col)
+{
+    float min, max;
+    for(int j=0; j<col; j++)
+    {   
+        // Find min, max
+        min=1000000;
+        max=-1000000;
+        for(int i=0; i<row; i++)
+        {
+            if (m[i][j] < min)
+                min = m[i][j];
+            if (m[i][j] > max)
+                max = m[i][j];
+        }
+        // Normalize [-1, 1]
+        for(int i=0; i<row; i++)
+            m[i][j] = (2.0*(m[i][j]-min)/(max-min))-1.0;
+    }
+}
+
 int main()
 {
     float learning_rate = 0.1;
 
     // Load data into 2D-array
-    // char *data_name="kc_house_data.csv", *label_name="kc_house_data_label.csv";
-    // float **X=load_data(data_name), **Y=load_data(label_name);
-    // int *row_col = get_csv_row_col(data_name), rowX, colX, rowY, colY;
-    // rowX = rowY = row_col[0];
-    // colX = row_col[1];
-    // colY = 1;
-    // free(row_col);
+    char *data_name="kc_house_data.csv", *label_name="kc_house_data_label.csv";
+    float **X=load_data(data_name), **Y=load_data(label_name);
+    int *row_col = get_csv_row_col(data_name), rowX, colX, rowY, colY;
+    rowX = rowY = row_col[0];
+    colX = row_col[1];
+    colY = 1;
+    free(row_col);
 
-    int rowX = 11, rowY = 11, colX = 1, colY = 1;
-    float **X=Matrix_create(11, 1, 0), **Y=Matrix_create(11, 1, 0);
-    X[0][0] = 65;
-    X[1][0] = 67;
-    X[2][0] = 71;
-    X[3][0] = 71;
-    X[4][0] = 66;
-    X[5][0] = 75;
-    X[6][0] = 67;
-    X[7][0] = 70;
-    X[8][0] = 71;
-    X[9][0] = 69;
-    X[10][0] = 69;
-
-    Y[0][0] = 175;
-    Y[1][0] = 133;
-    Y[2][0] = 185;
-    Y[3][0] = 163;
-    Y[4][0] = 126;
-    Y[5][0] = 198;
-    Y[6][0] = 153;
-    Y[7][0] = 163;
-    Y[8][0] = 159;
-    Y[9][0] = 151;
-    Y[10][0] = 159;
+    // Normalize data
+    Normalize(X, rowX, colX);
 
     // Initialize weight
     int rowW = colX;
@@ -206,7 +204,7 @@ int main()
     int colB = colY;
     float **B = Matrix_create(rowB, colB, 1);
 
-    printf("\nX[%d][%d], W[%d][%d], Y[%d][%d], b[%d][%d]\n", rowX, colX, rowW, colW, rowY, colY, rowB, colB);
+    printf("\nX[%d][%d], W[%d][%d], Y[%d][%d], b[%d][%d]\n\n", rowX, colX, rowW, colW, rowY, colY, rowB, colB);
 
     // Define backward variables
     float **Y_hat = Matrix_create(rowY, colY, 0);
@@ -215,44 +213,47 @@ int main()
     float **dL_dB = Matrix_create(rowB, colB, 0);
     float loss;
 
-    for(int iteration=0; iteration<10; iteration++)
+    for(int iteration=0; iteration<100000; iteration++)
     {
         // Forward propagation
         Matrix_multiplication(X, W, Y_hat, rowY, colY, colX);
         Matrix_addition(Y_hat, B, Y_hat, rowY, colY);
 
         // Backward propagation
-        
         // dL_dW
         Matrix_subtraction(Y_hat, Y, Y_hat_sub_Y, rowY, colY); // (Y'-Y) # ' is hat
         float **X_T = Matrix_transpose(X, rowX, colX); // X transpose
         Matrix_multiplication(X_T, Y_hat_sub_Y, dL_dW, rowW, colW, rowY); // X x (Y'-Y)
-        Matrix_constant_multiplication(dL_dW, rowW, colW, learning_rate*2.0/(float)rowX);
+        Matrix_constant_multiplication(dL_dW, rowW, colW, learning_rate*2.0/rowX);
         // Update W
         Matrix_subtraction(W, dL_dW, W, rowW, colW);
 
         // dL_dB
         Matrix_copy(dL_dB, Y_hat_sub_Y, rowB, colB);
-        Matrix_constant_multiplication(dL_dB, rowB, colB, learning_rate*2.0/(float)rowX);
+        Matrix_constant_multiplication(dL_dB, rowB, colB, learning_rate*2.0/rowX);
         // Update B
         Matrix_subtraction(B, dL_dB, B, rowB, colB);
 
         // Show loss
-        loss = 0;
-        for(int n=0; n<rowY; n++)
+        if ((iteration%500) == 0)
         {
-            loss+=(Y_hat_sub_Y[n][0]*Y_hat_sub_Y[n][0]);
+            loss = 0;
+            for(int n=0; n<rowY; n++)
+                loss+=(Y_hat_sub_Y[n][0]*Y_hat_sub_Y[n][0]);
+            loss/=rowX;
+            printf("Iteration(%05d), Loss: %f\n", iteration, loss);
         }
-        loss/=rowX;
-        printf("Iteration(%03d), Loss: %f\n", iteration+1, loss);
-
         // Free transposed matrix
         for(int n=0; n<colX; n++)
         {
-            free(X_T[n]);    
+            free(X_T[n]);
         }
         free(X_T);
     }
 
+    Matrix_multiplication(X, W, Y_hat, rowY, colY, colX);
+    Matrix_addition(Y_hat, B, Y_hat, rowY, colY);
+    for(int i=0;  i<100; i++)
+        printf("%15.2f, %15.2f\n", Y_hat[i][0], Y[i][0]);
     return 1;
 }
